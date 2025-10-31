1d835f97218073df76291acbb82c9f3024b82a2c
AbdujalolovaE'zoza <karimovaezoza2006@gmail.com>
2025-10-31 16:38:05 +0500

    4-month exams

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let data = [
+
+    
+]
+fetch("https://6904846c6b8dabde49640f3d.mockapi.io/name/ex", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        title: "Lyubuoy soz",
+    })
+})
